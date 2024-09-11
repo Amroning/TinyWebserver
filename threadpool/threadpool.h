50149acd262f22e4ae930beb73543814c602b593
Amroning <645519154@qq.com>
2024-09-11 20:14:33 +0800

    完善线程池

--- threadpool/threadpool.h
+++ threadpool/threadpool.h
@@ -14,18 +14,18 @@ public:
     threadpool(int actor_model, connection_pool* connPool, int thread_number = 8, int max_request = 10000);
     ~threadpool();
     bool append(T* request, int state);
-    bppl append_p(T* request);
+    bool append_p(T* request);
 
 private:
     //工作线程运行的函数，不断从工作队列中取出任务并执行
     static void* worker(void* arg);
-    void run;
+    void run();
 
 private:
     int m_thread_number;            //线程池中的线程数
     int m_max_request;              //请求队列中允许的最大请求数
     pthread_t* m_threads;           //描述线程池的数组，大小为m_thread_number
-    list<T*> m_workqueue;           //请求队列
+    std::list<T*>m_workqueue;       //请求队列
     locker m_queuelocker;           //保护请求队列的互斥锁
     sem m_queuestat;                //信号量  是否有任务需要处理
     connection_pool* m_connPool;    //数据库
@@ -53,6 +53,66 @@ threadpool<T>::threadpool(int actor_model, connection_pool* connPool, int thread
 template<typename T>
 threadpool<T>::~threadpool() {
     delete[] m_threads;
+}
+
+template<typename T>
+bool threadpool<T>::append(T* request, int state) {
+    m_queuelocker.lock();
+    //检查当前请求队列是否已满
+    if (m_workqueue.size() >= m_max_request) {
+        m_queuelocker.unlock();
+        return false;
+    }
+    //添加请求至队列中
+    request->m_state = state;
+    m_workqueue.push_back(request);
+    m_queuelocker.unlock();
+    m_queuestat.post();
+    return true;
+}
+
+template<typename T>
+bool threadpool<T>::append_p(T* request) {
+    m_queuelocker.lock();
+    //检查当前请求队列是否已满
+    if (m_workqueue.size() >= m_max_request) {
+        m_queuelocker.unlock();
+        return false;
+    }
+
+    m_workqueue.push_back(request);
+    m_queuelocker.unlock();
+    m_queuestat.post();
+    return true;
+}
+
+template<typename T>
+void* threadpool<T>::worker(void* arg) {
+    threadpool* pool = (threadpool*)arg;
+    pool->run();
+    return pool;
+}
+
+template<typename T>
+void threadpool<T>::run() {
+    while (true) {
+        m_queuestat.wait();
+        m_queuelocker.lock();
+        //检查请求队列是否为空
+        if (m_workqueue.empty()) {
+            m_queuelocker.unlock();
+            continue;
+        }
+        //取出请求开始处理
+        T* request = m_workqueue.front();
+        m_workqueue.pop_front();
+        m_queuelocker.unlock();
+
+        if (!request)
+            continue;
+
+        
+    }
 }
 
 #endif
